e69afd879a1983509e3e30bc07aa3a92b04ec31a
Michael Minichino <mminichino@mac.com>
2021-10-01 03:12:22 +0000

    Added key name field

--- set-dns-key.sh
+++ set-dns-key.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 [ ! -d $HOME/.dns ] && mkdir $HOME/.dns && chmod 700 $HOME/.dns
 
+echo -n "Key Name : "
+read KEYNAME
 echo -n "DNS Key : "
 read DNSKEY
 
-echo "{ \"dnskey\": \""$DNSKEY"\" }" | jq . > $HOME/.dns/dns.key
+echo "{ \"keyname\": \""$KEYNAME"\", \"dnskey\": \""$DNSKEY"\" }" | jq . > $HOME/.dns/dns.key
 chmod 600 $HOME/.dns/dns.key
